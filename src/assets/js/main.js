8294046a3861d2dd350b6020c15a930fb2fb72de
Trevor Sorel <twilliamsorel@gmail.com>
2020-02-10 18:03:28 -0800

    starting on homepage

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,9 +1,10 @@
 // MAIN NAV
 (function () {
   var nav = document.querySelector('nav');
+  var navWrapper = document.querySelector('.nav-container');
 
   window.addEventListener('scroll', function () {
-    if (nav.getBoundingClientRect().top < 1 && window.pageYOffset > 0) {
+    if (nav.getBoundingClientRect().top < 1 && window.pageYOffset > 0 && navWrapper.getBoundingClientRect().top < 1) {
       nav.classList.add('fixed')
     } else {
       nav.classList.remove('fixed');
