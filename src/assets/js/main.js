366daceb10772f20a24d72066162d8127bd788ea
Trevor Sorel <twilliamsorel@gmail.com>
2020-02-11 16:11:36 -0800

    homepage

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,11 +3,14 @@
   var nav = document.querySelector('nav');
   var navWrapper = document.querySelector('.nav-container');
 
-  window.addEventListener('scroll', function () {
+  function fixNav () {
     if (nav.getBoundingClientRect().top < 1 && window.pageYOffset > 0 && navWrapper.getBoundingClientRect().top < 1) {
       nav.classList.add('fixed')
     } else {
       nav.classList.remove('fixed');
     }
-  });
+  };
+
+  window.addEventListener('scroll', fixNav);
+  window.addEventListener('resize', fixNav);
 }());
